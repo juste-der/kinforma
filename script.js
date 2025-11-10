e80508fab5881618b52dd4f67f81dada40c2b1ce
Juste <juste.derk@gmail.com>
2025-11-10 12:33:39 +0100

    changed icon structure, aded svg and id for every icon, initialised style in nav menu

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ closeBtn.addEventListener("click", (e) => {
   sidenav.classList.remove("open");
   menuIcon.src = "assets/icons/hamburger.svg";
 });
+
+// const sidenav = document.getElementById("sidenav");
+// const menuToggle = document.getElementById("menuToggle");
+// const menuIcon = document.querySelector("#menuIcon use");
+
+// menuToggle.addEventListener("click", () => {
+//   sidenav.classList.toggle("open");
+
+//   if (sidenav.classList.contains("open")) {
+//     // Switch to "X" icon
+//     menuIcon.setAttribute("href", "assets/icons/icons.svg#icon-close");
+//   } else {
+//     // Switch back to hamburger
+//     menuIcon.setAttribute("href", "assets/icons/icons.svg#icon-hamburger");
+//   }
+// });
